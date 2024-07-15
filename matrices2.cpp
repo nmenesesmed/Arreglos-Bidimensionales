04afbe42aefeff60b2eb5704248fff1591072ee9
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-07-15 09:36:55 -0500

    matrices2

---
+++ matrices2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Ingrese el tamaño de la matriz cuadrada: ";
+    cin >> n;
+
+    int matriz[n][n];
+    cout << "Ingrese los elementos de la matriz:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matriz[i][j];
+        }
+    }
+
+    int suma_diag_secundaria = 0;
+    for (int i = 0; i < n; ++i) {
+        suma_diag_secundaria += matriz[i][n - 1 - i];
+    }
+
+    cout << "La suma de los elementos de la diagonal secundaria es: " << suma_diag_secundaria << endl;
+
+    return 0;
+}
