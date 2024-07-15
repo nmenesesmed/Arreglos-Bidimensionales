5dbdd7183759eb903e72fc210be807e6f64a54bc
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-07-15 10:21:40 -0500

    matrices2
    Comentarios incluidos para especificar que significa el conjunto de estructuras

--- matrices2.cpp
+++ matrices2.cpp
@@ -7,6 +7,8 @@ int main() {
     cin >> n;
 
     int matriz[n][n];
+
+    // Solicitar al usuario los elementos de la matriz
     cout << "Ingrese los elementos de la matriz:" << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -14,11 +16,13 @@ int main() {
         }
     }
 
+    // Calcular la suma de los elementos de la diagonal secundaria
     int suma_diag_secundaria = 0;
     for (int i = 0; i < n; ++i) {
         suma_diag_secundaria += matriz[i][n - 1 - i];
     }
 
+    // Mostrar el resultado de la suma de la diagonal secundaria
     cout << "La suma de los elementos de la diagonal secundaria es: " << suma_diag_secundaria << endl;
 
     return 0;
